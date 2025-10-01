2266e1eff9dcf19968467053fa0f36f0919a302d
WoMingBoazCheung <boazcheung2003@gmail.com>
2025-10-01 13:18:49 -0400

    fixed key

--- infra/backend.tf
+++ infra/backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
     bucket         = "terraform-state-bucket-immigreat"
-    key            = "s3://terraform-state-bucket-immigreat/terraform/terraform.tfstate"
+    key            = "terraform/terraform.tfstate"
     region         = "us-east-1"
     encrypt        = true
     dynamodb_table = "terraform-state-lock"
