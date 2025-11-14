80234cd1fa8d60680561aa89b5297c515b7f5b17
Alice Sedgwick <101151775+AxisShine@users.noreply.github.com>
2025-11-14 11:53:33 -0500

    function url changed

--- load_test/locust.py
+++ load_test/locust.py
@@ -3,7 +3,7 @@ from locust import HttpUser, task, between
 class RAGUser(HttpUser):
     wait_time = between(1, 3)
 
-    host = "https://e273eit4pnypmaeb57kdkvy6qy0wngwv.lambda-url.us-east-1.on.aws/"
+    host = "https://e3r43imacumt3txigpboq7uhbe0kyybv.lambda-url.us-east-1.on.aws"
 
     @task
     def test_lambda(self):
